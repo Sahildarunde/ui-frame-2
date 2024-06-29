b5e385e56fb69e6ef4193bcd50a25aa5a16071e5
Sahil Darunde <sahildarunde007@example.com>
2024-06-29 10:38:11 +0530

    dynamic profiling

--- components/HeroComp.tsx
+++ components/HeroComp.tsx
@@ -1,10 +1,10 @@
 import Left from "./Left";
 import Right from "./Right";
 
-export default function HeroComp(){
+export default function HeroComp({selectedImage , onImageChange}: any){
     return (
         <div className="flex flex-col md:flex-row">
-            <Left />
+            <Left selectedImage={selectedImage} onImageChange={onImageChange}/>
             <Right />
         </div>
     );
--- components/Main.tsx
+++ components/Main.tsx
@@ -1,17 +1,23 @@
-"use client"
+"use client";
 
-import HeroComp from "./HeroComp";
+import { ChangeEvent, useState } from 'react';
 import Navbar from "./Navbar";
+import HeroComp from './HeroComp';
 
-export default function Main(){
+export default function Main() {
+    const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      const imageFile = event.target.files[0];
+      setSelectedImage(URL.createObjectURL(imageFile));
+    }
+  };
+
     return (
         <div className="h-screen w-screen">
-            <div>
-                <Navbar />
-            </div>
-            <div>
-                <HeroComp />
-            </div>
+            <Navbar selectedImage={selectedImage} />
+            <HeroComp selectedImage={selectedImage} onImageChange={handleImageChange} />
         </div>
     );
 }
